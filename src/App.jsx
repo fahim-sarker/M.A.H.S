621fe1e0cf642caba27cc6bbc84a65e02134b687
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-09-03 22:55:55 +0600

    myproject

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+   <>
+    <div className="container mx-auto">
+      <h2 className="text-[50px] text-red-600">Matha Bhanga Adarsha High School</h2>
+    </div>
+   </>
+  )
+}
+
+export default App
